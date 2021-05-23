d296500d63249c5fbd676298467e9e4e9f7161e5
Uri Nissenkorn <urin@mail.tau.ac.il>
2021-05-23 12:58:06 +0300

    measurements

--- hw1ds/practical/src/il/ac/tau/cs/data_structures/practical1/mesurements.java
+++ hw1ds/practical/src/il/ac/tau/cs/data_structures/practical1/mesurements.java
@@ -3,6 +3,7 @@ package il.ac.tau.cs.data_structures.practical1;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.Random;
+import java.util.stream.IntStream;
 
 public class mesurements {
     Random random =  new Random();
@@ -10,15 +11,17 @@ public class mesurements {
     public static void main(String[] args) {
         mesurements m = new mesurements();
         m.first();
+        System.out.println("=====================");
+        m.second();
     }
 
     public void first() {
         for (int i=1; i<=5; i++) {
             AVLTreeOur treeOur = new AVLTreeOur();
-            long timeElapsed = 0;
-            long succTimeElapsed = 0;
-            long start;
-            long finish;
+            double timeElapsed = 0;
+            double succTimeElapsed = 0;
+            double start;
+            double finish;
             while (treeOur.size()<500*i) {
                 treeOur.insert(random.nextInt(1000000000), random.nextBoolean());
             }
@@ -64,6 +67,102 @@ public class mesurements {
 
             System.out.println(i+": all: succ:"+succTimeElapsed/(500*i));
             System.out.println(i+": all: pref:"+timeElapsed/(500*i));
+        }
+    }
+
+    public  void second() {
+        for (int i=1; i<=5; i++) {
+            AVLTreeOur treeOur;
+            unbalancedAVL unbalancedTree;
+
+            double timeElapsed = 0;
+            double start;
+            double finish;
+
+            int[] heshbon  = IntStream.rangeClosed(0, 1000*i).toArray();
+
+            int[] balanced  = new int[1000*i];
+            int j = 2;
+            int layer = 2;
+            balanced[1] = 1 + 500*i;
+            while (j<1000*i) {
+                for (int k = 1; k <= layer; k++) {
+                    balanced[j] = (int) (balanced[j/2] + Math.pow(-1,k)*(500/layer));
+                    j++;
+                    if (j>=1000*i)
+                        break;
+                }
+                layer *= 2;
+            }
+
+            int[] rndm  = random.ints(1000*i, 1, Integer.MAX_VALUE ).toArray();
+
+
+
+            // balanced heshbon
+            treeOur = new AVLTreeOur();
+            start = System.nanoTime();
+            for (int k = 1; j < 1000*i; j++) {
+                treeOur.insert(heshbon[k],true);
+            }
+            finish = System.nanoTime();
+            timeElapsed = finish - start;
+            System.out.println(i+": balanced: heshbon:"+timeElapsed/(1000*i));
+
+            // unbalanced heshbon
+            unbalancedTree = new unbalancedAVL();
+            start = System.nanoTime();
+            for (int k = 1; j < 1000*i; j++) {
+                unbalancedTree.insert(heshbon[k],true);
+            }
+            finish = System.nanoTime();
+            timeElapsed = finish - start;
+            System.out.println(i+": unbalanced: heshbon:"+timeElapsed/(1000*i));
+
+
+
+            // balanced balanced
+            treeOur = new AVLTreeOur();
+            start = System.nanoTime();
+            for (int k = 1; j < 1000*i; j++) {
+                treeOur.insert(balanced[k],true);
+            }
+            finish = System.nanoTime();
+            timeElapsed = finish - start;
+            System.out.println(i+": balanced: balanced:"+timeElapsed/(1000*i));
+
+            // unbalanced balanced
+            unbalancedTree = new unbalancedAVL();
+            start = System.nanoTime();
+            for (int k = 1; j < 1000*i; j++) {
+                unbalancedTree.insert(balanced[k],true);
+            }
+            finish = System.nanoTime();
+            timeElapsed = finish - start;
+            System.out.println(i+": unbalanced: balanced:"+timeElapsed/(1000*i));
+
+
+
+            // balanced rndm
+            treeOur = new AVLTreeOur();
+            start = System.nanoTime();
+            for (int k = 1; j < 1000*i; j++) {
+                treeOur.insert(rndm[k],true);
+            }
+            finish = System.nanoTime();
+            timeElapsed = finish - start;
+            System.out.println(i+": balanced: rndm:"+timeElapsed/(1000*i));
+
+            // unbalanced rndm
+            unbalancedTree = new unbalancedAVL();
+            start = System.nanoTime();
+            for (int k = 1; j < 1000*i; j++) {
+                unbalancedTree.insert(rndm[k],true);
+            }
+            finish = System.nanoTime();
+            timeElapsed = finish - start;
+            System.out.println(i+": unbalanced: rndm:"+timeElapsed/(1000*i));
+
         }
     }
 }
